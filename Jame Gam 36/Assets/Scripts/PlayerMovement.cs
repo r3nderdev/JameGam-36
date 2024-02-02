e75a0a5835fb5dd587da1dfa33a9c5864a149360
Mixu <mikohurme@gmail.com>
2024-02-02 19:49:30 +0200

    Almost completed Final Level

--- Jame Gam 36/Assets/Scripts/PlayerMovement.cs
+++ Jame Gam 36/Assets/Scripts/PlayerMovement.cs
@@ -40,7 +40,7 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         // ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.1f, ground);
 
         MyInput();
         SpeedControl();
