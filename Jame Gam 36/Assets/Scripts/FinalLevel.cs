0a78cacf932465e64a7fd0c5832d364c08838cc7
Mixu <mikohurme@gmail.com>
2024-02-02 20:43:13 +0200

    Final level done

---
+++ Jame Gam 36/Assets/Scripts/FinalLevel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalLevel : MonoBehaviour
+{
+    public Book book;
+    public LevelFade fade;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (book.currentPage == 4)
+        {
+            // End game
+            fade.FadeToLevel(3);
+        }
+    }
+}
